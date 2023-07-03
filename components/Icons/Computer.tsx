064ede0c03dfb384bc061854312cb1f9638e466c
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-07-03 19:30:37 -0300

    Add site icon

---
+++ components/Icons/Computer.tsx
@@ -0,0 +1,21 @@
+export default function Computer() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 64 64"
+      width="32"
+      height="32"
+    >
+      <path
+        d="m1.42 38.13 61.08-.19v3.48a7 7 0 0 1-7 7H8.39a7 7 0 0 1-7-7V14.83a7 7 0 0 1 7-7h47.13a7 7 0 0 1 7 7v17.9"
+        stroke="currentColor"
+        strokeWidth="2"
+      />
+      <path
+        d="M28.25 48.4S28 54.59 22 56.23M35.93 48.4s.26 6.19 6.3 7.83M18.04 56.23h27.84M31.02 43h1.88"
+        stroke="currentColor"
+        strokeWidth="2"
+      />
+    </svg>
+  )
+}
--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,8 +1,16 @@
-import { Instagram, Twitter, Camera } from 'components/Icons'
+import { Instagram, Twitter, Camera, Computer } from 'components/Icons'
 
 function SocialLinks() {
   return (
     <div className="mt-2 flex items-center justify-center gap-3">
+      <a
+        href="https://willianjusten.com.br"
+        target="_blank"
+        className="font-semibold text-white/90 hover:text-white"
+        rel="noreferrer"
+      >
+        <Computer />
+      </a>
       <a
         href="https://twitter.com/Willian_justen"
         target="_blank"
