8f06b1317b4384c1d6100440f5236780816d2d8a
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-11-19 19:29:29 -0300

    Update cachedImages

--- utils/cachedImages.ts
+++ utils/cachedImages.ts
@@ -9,6 +9,18 @@ export default async function getResults() {
       .max_results(2000)
       .execute()
 
+    if (fetchedResults?.next_cursor) {
+      const moreResults = await cloudinary.v2.search
+        .sort_by('folder', 'asc')
+        .next_cursor(fetchedResults?.next_cursor)
+        .max_results(2000)
+        .execute()
+
+      fetchedResults.resources = fetchedResults.resources.concat(
+        moreResults.resources
+      )
+    }
+
     cachedResults = fetchedResults
   }
 
